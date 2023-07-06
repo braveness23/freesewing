71afcd4c64770a89f0c68f091c51bd1373bb2129
Dave Schwinn <braveness23@gmail.com>
2023-07-06 16:50:26 -0400

    add bib

--- designs/dirk/src/index.mjs
+++ designs/dirk/src/index.mjs
@@ -5,12 +5,13 @@ import { data } from '../data.mjs'
 // Parts
 import { frontTrouser } from './frontTrouser.mjs'
 import { rearTrouser } from './rearTrouser.mjs'
+import { bib } from './bib.mjs'
 
 // Create new design
 const Dirk = new Design({
   data,
-  parts: [frontTrouser, rearTrouser],
+  parts: [frontTrouser, rearTrouser, bib],
 })
 
 // Named exports
-export { frontTrouser, rearTrouser, Dirk }
+export { frontTrouser, rearTrouser, bib, Dirk }
