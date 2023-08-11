bc03c1104a88a9eed7ed95e7ba8c63a19e6d51a7
Dave Schwinn <braveness23@gmail.com>
2023-08-11 18:48:58 -0400

    wip

--- designs/dirk/src/bib.mjs
+++ designs/dirk/src/bib.mjs
@@ -2,84 +2,110 @@ import { pluginBundle } from '@freesewing/plugin-bundle'
 
 export const bib = {
   name: 'bib',
-  //measurements: ['waist', 'waistToFloor'],
-  options: {
-    waistEase: {
-      pct: 1,
-      min: 0,
-      max: 5,
-      menu: 'fit',
-    },
-  },
   plugins: [pluginBundle],
-  draft: draftBib,
-}
+  measurements: ['waist', 'waistToFloor'],
+  options: {
+    // TODO: set these to reasonable values
+    bodyEase: { pct: 21, min: 21, max: 50, menu: 'fit' },
+    bibTopWidth: { pct: 1, min: 0, max: 5, menu: 'style' },
+    waistEase: { pct: 1, min: 0, max: 5, menu: 'fit' },
+    shiftLength: { pct: 2, min: 0, max: 20, menu: 'style' },
+  },
 
-function draftBib({
-  points,
-  Point,
-  paths,
-  Path,
-  options,
-  complete,
-  paperless,
-  measurements,
-  macro,
-  sa,
-  part,
-}) {
-  points.topLeft = new Point(-100, 0)
-  points.leftFold = new Point(-200, 300)
-  points.waistLeft = new Point(-200, 340)
-  points.waistRight = new Point(200, 340)
-  points.rightFold = new Point(200, 300)
-  points.topRight = new Point(100, 0)
+  draft: function draftBib({
+    points,
+    Point,
+    paths,
+    Path,
+    Snippet,
+    snippets,
+    options,
+    complete,
+    paperless,
+    measurements,
+    macro,
+    sa,
+    part,
+  }) {
+    //body
+    const lengthBody =
+      (measurements.waistToKnee + measurements.hpsToWaistBack) * (1 + options.shiftLength)
 
-  paths.seam = new Path()
-    .move(points.topLeft)
-    .line(points.leftFold)
-    .line(points.waistLeft)
-    .line(points.waistRight)
-    .line(points.rightFold)
-    .line(points.topRight)
-    .line(points.topLeft)
-    .close()
-    .attr('class', 'fabric')
+    const workingHip =
+      measurements.chest > measurements.hips ? measurements.chest / 2 : measurements.hips / 2
 
-  // Complete?
-  if (complete) {
-    // TODO: Title here
-    // TODO: Logo here
+    const widthBody = workingHip * (1 + options.bodyEase)
 
-    if (sa) {
-      paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa')
+    const maxLength =
+      lengthBody > measurements.waistToFloor + measurements.hpsToWaistBack
+        ? measurements.waistToFloor + measurements.hpsToWaistBack
+        : lengthBody
+
+    points.topLeft = new Point(-100, 0)
+    points.leftFold = new Point(-200, 300)
+    points.waistLeft = new Point(-200, 340)
+    points.waistRight = new Point(200, 340)
+    points.rightFold = new Point(200, 300)
+    points.topRight = new Point(100, 0)
+
+    paths.seam = new Path()
+      .move(points.topLeft)
+      .line(points.leftFold)
+      .line(points.waistLeft)
+      .line(points.waistRight)
+      .line(points.rightFold)
+      .line(points.topRight)
+      .line(points.topLeft)
+      .close()
+      .attr('class', 'fabric')
+
+    // Complete?
+    if (complete) {
+      // Logo
+      points.logo = new Point(0, 150)
+      // points.logo = points.middle.shiftTowards(points.middleHem, lengthBody / 4)
+      snippets.logo = new Snippet('logo', points.logo)
+
+      // Title
+      points.title = new Point(0, 250)
+      macro('title', {
+        at: points.title,
+        nr: 1,
+        title: 'bib',
+      })
+
+      // TODO: Logo here
+
+      if (sa) {
+        paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa')
+      }
     }
-  }
 
-  // Paperless?
-  if (paperless) {
-    macro('hd', {
-      from: points.topLeft,
-      to: points.topRight,
-      y: points.topLeft.y + sa + 15,
-    })
-    macro('hd', {
-      from: points.waistLeft,
-      to: points.waistRight,
-      y: points.waistLeft.y + sa + 15,
-    })
+    // Paperless?
+    if (paperless) {
+      macro('hd', {
+        from: points.topLeft,
+        to: points.topRight,
+        y: points.topLeft.y + sa + 15,
+      })
+      macro('hd', {
+        from: points.waistLeft,
+        to: points.waistRight,
+        y: points.waistLeft.y + sa + 15,
+      })
 
-    // macro('vd', {
-    //   from: points.waistOut,
-    //   to: points.foldOut,
-    //   x: points.waistOut.x + sa + 15,
-    // })
-    // macro('vd', {
-    //   from: points.topIn,
-    //   to: points.waistIn,
-    //   x: points.topIn.x + sa + 15,
-    // })
-  }
+      // macro('vd', {
+      //   from: points.waistOut,
+      //   to: points.foldOut,
+      //   x: points.waistOut.x + sa + 15,
+      // })
+      // macro('vd', {
+      //   from: points.topIn,
+      //   to: points.waistIn,
+      //   x: points.topIn.x + sa + 15,
+      // })
+    }
 
-  return part
+    return part
+  },
 }
